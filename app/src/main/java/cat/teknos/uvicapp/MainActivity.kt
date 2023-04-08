a94d80394a0f8c65588c6b6092eebc62218ce8a8
enriqueseor <enriqueseor@gmail.com>
2023-04-08 23:58:11 +0100

    don't know why is not working

--- app/src/main/java/cat/teknos/uvicapp/MainActivity.kt
+++ app/src/main/java/cat/teknos/uvicapp/MainActivity.kt
@@ -2,10 +2,33 @@ package cat.teknos.uvicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.events -> replaceFragment(EventsFragment())
+                R.id.card -> replaceFragment(CardFragment())
+                R.id.statements -> replaceFragment(StatementsFragment())
+                R.id.launcher -> replaceFragment(LauncherFragment())
+                R.id.bbva -> replaceFragment(BbvaFragment())
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
 }
