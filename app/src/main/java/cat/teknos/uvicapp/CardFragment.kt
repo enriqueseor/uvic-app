92829244238fdad12be6733b626d878c07d81f8a
enriqueseor <enriqueseor@gmail.com>
2023-05-15 18:25:48 +0100

    another type of animation with less fade

--- app/src/main/java/cat/teknos/uvicapp/CardFragment.kt
+++ app/src/main/java/cat/teknos/uvicapp/CardFragment.kt
@@ -7,10 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 
 class CardFragment : Fragment() {
+
+    private var isImageVisible = false
+    private lateinit var fadeInAnimation: Animation
+    private lateinit var fadeOutAnimation: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +24,34 @@ class CardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_card, container, false)
-        val myImage = rootView.findViewById<ImageView>(R.id.icon_sports_green)
 
-        val animation = AlphaAnimation(0.0f, 1.0f) // from fully transparent to fully opaque
-        animation.duration = 500 // duration in milliseconds
-        animation.interpolator = LinearInterpolator() // animation style
-        animation.repeatCount = Animation.INFINITE // repeat indefinitely
-        animation.repeatMode = Animation.REVERSE // reverse the animation at the end
+        val view = inflater.inflate(R.layout.fragment_card, container, false)
 
-        myImage.startAnimation(animation)
+        val iconSportsGreen: ImageView? = view.findViewById(R.id.icon_sports_green)
+        fadeInAnimation = AlphaAnimation(0f, 1f).apply {
+            duration = 500
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+                    iconSportsGreen?.visibility = if (isImageVisible) View.VISIBLE else View.INVISIBLE
+                    isImageVisible = !isImageVisible
+                    iconSportsGreen?.startAnimation(fadeOutAnimation)
+                }
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+        }
+        fadeOutAnimation = AlphaAnimation(1f, 0f).apply {
+            duration = 500
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+                    iconSportsGreen?.startAnimation(fadeInAnimation)
+                }
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+        }
+        iconSportsGreen?.startAnimation(fadeInAnimation)
 
-        return rootView
+        return view
     }
 }
