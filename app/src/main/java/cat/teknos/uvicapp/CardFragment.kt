8e9ec55d5391fb4fe2df4d885a45a484ba82018f
enriqueseor <enriqueseor@gmail.com>
2023-05-15 18:30:48 +0100

    visible invisible handler added, icon_sports_green

--- app/src/main/java/cat/teknos/uvicapp/CardFragment.kt
+++ app/src/main/java/cat/teknos/uvicapp/CardFragment.kt
@@ -1,19 +1,19 @@
 package cat.teknos.uvicapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
 import android.widget.ImageView
 
 class CardFragment : Fragment() {
 
     private var isImageVisible = false
-    private lateinit var fadeInAnimation: Animation
-    private lateinit var fadeOutAnimation: Animation
+    private lateinit var handler: Handler
+    private lateinit var toggleVisibilityRunnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,34 +24,24 @@ class CardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_card, container, false)
-
         val iconSportsGreen: ImageView? = view.findViewById(R.id.icon_sports_green)
-        fadeInAnimation = AlphaAnimation(0f, 1f).apply {
-            duration = 500
-            setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {}
-                override fun onAnimationEnd(animation: Animation?) {
-                    iconSportsGreen?.visibility = if (isImageVisible) View.VISIBLE else View.INVISIBLE
-                    isImageVisible = !isImageVisible
-                    iconSportsGreen?.startAnimation(fadeOutAnimation)
-                }
-                override fun onAnimationRepeat(animation: Animation?) {}
-            })
+        handler = Handler(Looper.getMainLooper())
+        toggleVisibilityRunnable = Runnable {
+            iconSportsGreen?.visibility = if (isImageVisible) View.VISIBLE else View.INVISIBLE
+            isImageVisible = !isImageVisible
+            scheduleToggleVisibility()
         }
-        fadeOutAnimation = AlphaAnimation(1f, 0f).apply {
-            duration = 500
-            setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {}
-                override fun onAnimationEnd(animation: Animation?) {
-                    iconSportsGreen?.startAnimation(fadeInAnimation)
-                }
-                override fun onAnimationRepeat(animation: Animation?) {}
-            })
-        }
-        iconSportsGreen?.startAnimation(fadeInAnimation)
-
+        scheduleToggleVisibility()
         return view
+    }
+
+    private fun scheduleToggleVisibility() {
+        handler.postDelayed(toggleVisibilityRunnable, 500)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        handler.removeCallbacks(toggleVisibilityRunnable)
     }
 }
