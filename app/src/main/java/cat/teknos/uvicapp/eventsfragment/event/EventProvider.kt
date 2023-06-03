499034c8407488fc3426d96a8b5c54c039df90ba
enriqueseor <enriqueseor@gmail.com>
2023-06-03 20:51:16 +0100

    rvEvents polishing and dark theme supressed

--- app/src/main/java/cat/teknos/uvicapp/eventsfragment/event/EventProvider.kt
+++ app/src/main/java/cat/teknos/uvicapp/eventsfragment/event/EventProvider.kt
@@ -3,24 +3,19 @@ package cat.teknos.uvicapp.eventsfragment.event
 object EventProvider {
     val eventList = listOf(
         Event(
-            "2",
-            "June",
-            "Concert de Gemma HUmet - el so de les cases"
-        ),
-        Event(
             "3",
             "June",
-            "Concert de Gemma HUmet - el so de les cases"
+            "Concert de Gemma Humet - el so de les cases"
         ),
         Event(
             "4",
             "June",
-            "Concert de Gemma HUmet - el so de les cases"
+            "Concert de Gemma Humet - el so de les cases"
         ),
         Event(
             "5",
             "June",
-            "Acte de graduaciío de la primera promoció d'estudiants del grau en Medicina de la UVic-UCC"
+            "Acte de graduacío de la primera promoció d'estudiants del grau en Medicina de la UVic-UCC"
         ),
         Event(
             "6",
@@ -35,12 +30,12 @@ object EventProvider {
         Event(
             "7",
             "June",
-            "Concert de Gemma HUmet - el so de les cases"
+            "Concert de la Coral de la UVic"
         ),
         Event(
             "7",
             "June",
-            "Concert de Gemma HUmet - el so de les cases"
+            "IV Congrès de l'Acció Social - Inclusió.cat"
         ),
     )
 }
