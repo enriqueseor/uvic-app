7eb208415ca62305df9e37bc7d929bc36d480044
enriqueseor <enriqueseor@gmail.com>
2023-06-20 19:04:19 +0100

    Action bar changing color semi-implemented

--- app/src/main/java/cat/teknos/uvicapp/bbvafragment/BbvaFragment.kt
+++ app/src/main/java/cat/teknos/uvicapp/bbvafragment/BbvaFragment.kt
@@ -1,16 +1,19 @@
 package cat.teknos.uvicapp.bbvafragment
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import cat.teknos.uvicapp.R
 
 class BbvaFragment : Fragment() {
 
+    private var actionBarColor: Int = 0
     private var statusBarColor: Int = 0
     private var colorChanged: Boolean = false
 
@@ -23,11 +26,16 @@ class BbvaFragment : Fragment() {
             val theme = contextThemeWrapper.theme
             val typedValue = TypedValue()
 
+            theme.resolveAttribute(com.google.android.material.R.attr.colorSecondary, typedValue, true)
+            val secondaryColor = typedValue.data
+            val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+            actionBar?.setBackgroundDrawable(ColorDrawable(secondaryColor))
+
             theme.resolveAttribute(com.google.android.material.R.attr.colorSecondaryVariant, typedValue, true)
             val secondaryVariantColor = typedValue.data
-
             statusBarColor = requireActivity().window.statusBarColor
             requireActivity().window.statusBarColor = secondaryVariantColor
+
             colorChanged = true
         }
     }
@@ -36,6 +44,8 @@ class BbvaFragment : Fragment() {
         super.onPause()
 
         if (colorChanged) {
+            val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+            actionBar?.setBackgroundDrawable(ColorDrawable(com.google.android.material.R.attr.colorPrimary))
             requireActivity().window.statusBarColor = statusBarColor
             colorChanged = false
         }
