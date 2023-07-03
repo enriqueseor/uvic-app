ce043787bccd1dd97242e3aa572501d2b9a8d678
enriqueseor <enriqueseor@gmail.com>
2023-07-03 17:23:05 +0100

    card timer added

--- app/src/main/java/cat/teknos/uvicapp/cardfragment/CardFragment.kt
+++ app/src/main/java/cat/teknos/uvicapp/cardfragment/CardFragment.kt
@@ -7,24 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import cat.teknos.uvicapp.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CardFragment : Fragment() {
 
     private var isImageVisible = false
     private lateinit var handler: Handler
+    private lateinit var tvTimer: TextView
     private lateinit var toggleVisibilityRunnable: Runnable
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {}
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_card, container, false)
         val iconSportsGreen: ImageView? = view.findViewById(R.id.icon_sports_green)
         handler = Handler(Looper.getMainLooper())
@@ -34,7 +34,20 @@ class CardFragment : Fragment() {
             scheduleToggleVisibility()
         }
         scheduleToggleVisibility()
+        tvTimer = view.findViewById(R.id.tvTimer)
+        handler = Handler(Looper.getMainLooper())
+        handler.post(updateClockRunnable)
         return view
+    }
+
+    private val updateClockRunnable = object : Runnable {
+        override fun run() {
+            val currentDate = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(Date())
+            val currentTime = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+            val dateTime = "$currentDate - $currentTime"
+            tvTimer.text = dateTime
+            handler.postDelayed(this, 1000) // Update every second
+        }
     }
 
     private fun scheduleToggleVisibility() {
@@ -44,5 +57,6 @@ class CardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         handler.removeCallbacks(toggleVisibilityRunnable)
+        handler.removeCallbacks(updateClockRunnable)
     }
 }
