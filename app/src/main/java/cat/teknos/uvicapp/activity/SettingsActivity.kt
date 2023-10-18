b5ccd44e46a3ccc31ed4ff86ffc659029655112b
enriqueseor <enriqueseor@gmail.com>
2023-10-18 12:44:21 +0100

    settings activity back arrow working

--- app/src/main/java/cat/teknos/uvicapp/activity/SettingsActivity.kt
+++ app/src/main/java/cat/teknos/uvicapp/activity/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package cat.teknos.uvicapp.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import cat.teknos.uvicapp.R
@@ -17,6 +18,16 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
